d2918adbf2f5208fb09ac0cbf1608306a5f9b927
Leonardo <>
2023-11-30 10:48:43 +0100

    Primer commit

---
+++ src/main/java/prueba/pruebagit/PruebaGit.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package prueba.pruebagit;
+
+/**
+ *
+ * @author Diurno
+ */
+public class PruebaGit {
+
+    public static void main(String[] args) {
+        System.out.println("Un cambio");
+        System.out.println("Hello World!");
+    }
+}
